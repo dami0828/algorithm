fc4fc79006ea7986f2695d0030eabe46558cf123
dami <devdami0828@gmail.com>
2023-12-21 20:59:49 +0900

    level_0

--- level_0/index.js
+++ level_0/index.js
@@ -248,3 +248,18 @@ function solution(a, b, c) {
   }
   return (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
 }
+
+// 마지막 두 원소
+// 정수 리스트 num_list가 주어질 때, 마지막 원소가 그전 원소보다 크면 마지막 원소에서 그전 원소를 뺀 값을
+// 마지막 원소가 그전 원소보다 크지 않다면 마지막 원소를 두 배한 값을 추가하여 return하도록 solution 함수를 완성해주세요.
+function solution(num_list) {
+  var answer = num_list;
+  const num1 = num_list[num_list.length - 1];
+  const num2 = num_list[num_list.length - 2];
+  if (num1 > num2) {
+    answer.push(num1 - num2);
+  } else {
+    answer.push(num1 * 2);
+  }
+  return answer;
+}
