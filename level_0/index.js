4b97da186de51568d369e360ed9379ce75c78514
dami <devdami0828@gmail.com>
2024-01-07 21:09:48 +0900

    level_0

--- level_0/index.js
+++ level_0/index.js
@@ -352,3 +352,12 @@ function solution(numLog) {
     })
     .join("");
 }
+
+// 카운트 업
+function solution(start_num, end_num) {
+  var answer = [];
+  for (let i = start_num; i <= end_num; i++) {
+    answer.push(i);
+  }
+  return answer;
+}
