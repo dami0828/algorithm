9bd6cd0845c63cb8f9e394c9060b398a9d0dc25c
dami <devdami0828@gmail.com>
2024-01-11 18:05:52 +0900

    level_0

--- level_0/index.js
+++ level_0/index.js
@@ -386,3 +386,9 @@ function solution(num_list) {
 
   return Math.max(evenSum, oddSum);
 }
+// 길이에 따른 연산
+function solution(num_list) {
+  return num_list.reduce((acc, cur) => {
+    return num_list.length >= 11 ? acc + cur : acc * cur;
+  });
+}
